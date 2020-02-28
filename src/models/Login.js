e61cfcd4a8b017d488147f585954f0c9b8bb48da
zhangpu <791458703@qq.com>
2020-02-28 11:22:54 +0800

    file

--- src/models/Login.js
+++
@@ -1,9 +0,0 @@
-import { postKeyValueRequest } from '@/utils/api'
-
-class Login {
-  async doLogin(loginForm) {
-    const res = await postKeyValueRequest('/doLogin', loginForm)
-    return res
-  }
-}
-export default new Login()
